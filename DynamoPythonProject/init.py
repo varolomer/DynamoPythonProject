1cecf197bcc79890aa4d8ee377485c1cfb574f98
Ömer Varol <omer_varol_25@hotmail.com>
2021-07-12 16:54:46 +0300

    Add project files.

---
+++ DynamoPythonProject/init.py
@@ -0,0 +1,30 @@
+
+#OV DynamoPythonProject
+
+#Import Common Language Runtime CLR
+import clr
+
+#Add and Import RevitServices Reference
+clr.AddReference("RevitServices")
+import RevitServices
+
+#Import DocumentManager and TransactionManager
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+#Get the current Revit Document
+doc = DocumentManager.Instance.CurrentDBDocument
+
+#Dynamo Inputs
+
+#Start a Transaction
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+#Code Here
+##
+
+#Finish Transaction
+TransactionManager.Instance.TransactionTaskDone()
+
+#Output
+OUT = "output"
