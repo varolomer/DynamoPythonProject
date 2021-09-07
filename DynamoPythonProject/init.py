85cdf6181775398a9203808bc96069a41b19f9c7
Ömer Varol <omer_varol_25@hotmail.com>
2021-09-07 12:14:03 +0300

    Fixes in init.py

--- DynamoPythonProject/init.py
+++ DynamoPythonProject/init.py
@@ -4,6 +4,9 @@
 import clr
 import sys
 
+#Append Custom Library
+#sys.path.append(r'path')
+
 #Add References
 clr.AddReference('RevitServices')
 clr.AddReference('ProtoGeometry')
@@ -12,7 +15,7 @@ clr.AddReference('RevitAPI')
 
 #Import Libraries
 import Revit #Nodes in Dynamo
-import Autodesk.Designscript.Geometry as Geometry #Import Dynamo Geometry Library to interact wit Revit
+import Autodesk.DesignScript.Geometry as Geometry #Import Dynamo Geometry Library to interact wit Revit
 import Autodesk.Revit.DB as DB #Import DB for direct RevitAPI calls. This requires unwrapping and wrapping
 
 #Import DocumentManager and TransactionManager
@@ -26,10 +29,10 @@ clr.ImportExtensions(Revit.GeometryConversion)
 #Get the current Revit Document
 doc = DocumentManager.Instance.CurrentDBDocument
 
-#Dynamo Inputs
+#############################################Dynamo Inputs
 input = IN[0]
 inputU = UnwrapElement(IN[0])
-
+#############################################
 #Start a Transaction
 TransactionManager.Instance.EnsureInTransaction(doc)
 
